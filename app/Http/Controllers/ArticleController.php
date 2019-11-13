fdd84a46a5c2a50f327059d6213fc2ad9616f8da
Onewkub <oldnew123@gmail.com>
2019-11-13 23:58:48 +0700

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Http\Resources\ArticleCollectionResource;
+use App\Http\Resources\ArticleResource;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function show(Article $article) : ArticleResource{
+        return new ArticleResource($article);
+    }
+    public function index(){
+        return new ArticleCollectionResource(Article::all());
+    }
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Article $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
+}
